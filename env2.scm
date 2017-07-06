487a000c59ed871b317d4fa4d43eb5c56c93dae2
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2017-07-06 13:35:19 -0700

    update extend env func

--- env2.scm
+++ env2.scm
@@ -1,6 +1,27 @@
 (define make-empty-env
 	(lambda () 
-		(list)
+		(lambda ()
+			(list)
+		)		
 	)
 )
 
+(define extend-env
+	(lambda (v val env)
+		(lambda ()
+			(cons (cons v val) env)
+		) 
+	)
+)
+
+(define search
+	(lambda (v env)
+		(if (equal? (car (car (env))) v)
+              #t
+              (if (equal? ((cdr (env))) '())
+                  #f
+                  (search v (cdr (env)))
+              )
+        )
+	)
+)
--- env2test.scm
+++ env2test.scm
@@ -1,3 +1,17 @@
 (load "env2.scm")
 
 (make-empty-env)
+
+((make-empty-env))
+
+(define protoenv
+	(extend-env 'key 1 (make-empty-env) )
+)
+
+(define alphaenv
+	(extend-env 'kiy 5 protoenv)
+)
+
+(search 'key protoenv)
+(search 'kay protoenv)
+(search 'key alphaenv)
